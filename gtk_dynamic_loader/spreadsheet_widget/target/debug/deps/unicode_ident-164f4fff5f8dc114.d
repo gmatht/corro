82612b7593ce32781fb64e6fabcf96399f73424f
John C. McCabe-Dansted <mccabedj@proton.me>
2026-06-04 02:35:27 +1200

    Squashed 'rustxWidgets/' content from commit bcbc69c
    git-subtree-dir: rustxWidgets
    git-subtree-split: bcbc69c558214c5a0565f11e0e2fb99617529ee7

---
+++ gtk_dynamic_loader/spreadsheet_widget/target/debug/deps/unicode_ident-164f4fff5f8dc114.d
@@ -0,0 +1,8 @@
+/home/gemini/dat/b/borg/src/borg/rust/target/debug/deps/unicode_ident-164f4fff5f8dc114.d: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/tables.rs
+
+/home/gemini/dat/b/borg/src/borg/rust/target/debug/deps/libunicode_ident-164f4fff5f8dc114.rlib: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/tables.rs
+
+/home/gemini/dat/b/borg/src/borg/rust/target/debug/deps/libunicode_ident-164f4fff5f8dc114.rmeta: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/tables.rs
+
+/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/lib.rs:
+/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/tables.rs:
